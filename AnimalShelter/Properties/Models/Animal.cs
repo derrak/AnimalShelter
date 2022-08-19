fa7922ea1ad3a06d91e46d47756c1b82e5b17cc8
Derrak Richard <derrak@gmail.com>
2022-08-19 12:12:09 -0700

    add animal model

---
+++ AnimalShelter/Properties/Models/Animal.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace AnimalShelter.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string AnimalName { get; set; }
+    [Required]
+    public string AnimalSpecies { get; set; }
+    [Required]
+    public string AnimalBreed { get; set; }
+    [Required]
+    public string Description { get; set; }
+    [Required]
+    public int UserId { get; set; }
+    [Required]
+    public DateTime DateCreated { get; set; }
+    [Required]
+    // public DateTime ModifiedDate { set; } = null;
+    public bool Availability { get; set; } = true;
+    [Required]
+  }
+}
